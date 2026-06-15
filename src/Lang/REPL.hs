afee9aaf2b4daef973516379850f86f7909f96b8
Dominic Orchard <dom.orchard@gmail.com>
2026-06-15 06:34:19 -0600

    change to different input method for repl

--- src/Lang/REPL.hs
+++ src/Lang/REPL.hs
@@ -12,8 +12,8 @@ import Lang.Kinding     (synthKind)
 import Lang.Options     (Option)
 
 import System.FilePath  (takeBaseName)
-import System.IO        (hFlush, stdout)
 import System.Console.Haskeline
+import Control.Monad.IO.Class (liftIO)
 
 data FileState = 
   FileState {
@@ -38,62 +38,67 @@ initialState = REPLState {
   }
 
 -- Main REPL
-replLoop :: REPLState -> IO REPLState
+replLoop :: REPLState -> InputT IO REPLState
 replLoop state = do
   line <- getInputLine (prompt state ++ "> ")
   case line of
-    ':':rest -> 
-      -- Command
-      case rest of
-        'q':_ -> return state
-        'h':_ -> do
-          printHelp
-          replLoop state
-        'l':' ':rest -> do
-          runResult <- run False rest 
-          case runResult of
-            Left err -> do
-              putStrLn err 
-              replLoop state
-            Right (ast, options, env, expr) -> do
-              displayResult expr
-              let fileState = FileState { filename = rest, ast = ast }
-              replLoop $ state { currentFile = Just fileState
-                                , prompt = takeBaseName rest
-                                , env = env
-                                , options = options }
+    Nothing -> return state
+    Just str -> case str of
+      ':':rest ->
+        -- Command
+        case rest of
+          'q':_ -> return state
+          'h':_ -> do
+            liftIO printHelp
+            replLoop state
+          'l':' ':path -> do
+            runResult <- liftIO $ run False path
+            case runResult of
+              Left err -> do
+                liftIO $ putStrLn err
+                replLoop state
+              Right (ast', opts, env', expr) -> do
+                liftIO $ displayResult expr
+                let fileState = FileState { filename = path, ast = ast' }
+                replLoop $ state { currentFile = Just fileState
+                                  , prompt = takeBaseName path
+                                  , env = env'
+                                  , options = opts }
 
-        't':' ':rest -> do
-          case parseExpr rest of
-            Left err   -> 
-              case parseType rest of
-                Left err -> putStrLn err
-                Right ty -> 
-                  case synthKind ty of
-                    Left err -> let ?srcFile = "<repl>" in putStrLn $ errorToString err
-                    Right (ty', kind) -> do
-                      if ty /= ty'
-                        then putStrLn $ "Elaborated type: " ++ pprint ty'
-                        else return ()
-                      putStrLn $ pprint kind
-            Right expr -> 
-              case synth [] expr of
-                Left err -> let ?srcFile = "<repl>" in putStrLn $ errorToString err
-                Right ty -> putStrLn $ pprint ty
-          replLoop state
-        rest -> do
-          putStrLn $ "Unknown command :" ++ rest
-          printHelp
-          replLoop state
-    -- not a command
-    rest -> do
-      case parseExpr rest of
-        Left err   -> putStrLn err
-        Right expr ->
-          case bigStep (env state) (options state) expr of
-            Left err  -> putStrLn err
-            Right var -> putStrLn $ pprint var
-      replLoop state
+          't':' ':rest' -> do
+            liftIO $ case parseExpr rest' of
+              Left err  -> putStrLn err
+              Right expr ->
+                case synth [] expr of
+                  Left err -> let ?srcFile = "<repl>" in putStrLn $ errorToString err
+                  Right ty -> putStrLn $ pprint ty
+            replLoop state
+
+          'k':' ': rest' -> do
+              liftIO $ case parseType rest' of
+                  Left err -> putStrLn err
+                  Right ty ->
+                    case synthKind ty of
+                      Left err -> let ?srcFile = "<repl>" in putStrLn $ errorToString err
+                      Right (ty', kind) -> do
+                        if ty /= ty'
+                          then putStrLn $ "Elaborated type: " ++ pprint ty'
+                          else return ()
+                        putStrLn $ pprint kind
+              replLoop state
+          rest' -> do
+            liftIO $ putStrLn $ "Unknown command :" ++ rest'
+            liftIO printHelp
+            replLoop state
+      -- not a command
+      rest -> do
+        liftIO $ case parseExpr rest of
+          Left err   -> putStrLn err
+          Right expr ->
+            case bigStep (env state) (options state) expr of
+              Left err  -> putStrLn err
+              Right var -> putStrLn $ pprint var
+        replLoop state
 
 displayResult :: Expr -> IO ()
 displayResult e = do
@@ -103,7 +108,7 @@ main :: IO ()
 main = do
   putStrLn $ ansi_bold <> banner <> ansi_reset
   putStrLn "Run :h for help"
-  _ <- replLoop initialState
+  _ <- runInputT defaultSettings (replLoop initialState)
   return ()
 
 printHelp :: IO ()
@@ -111,7 +116,7 @@ printHelp = do
   putStrLn "Commands:"
   putStrLn "    :h      - This message"
   putStrLn "    :t expr - Infer the type of an expression"
-  putStrLn "    :t type - Infer the type of a type"
+  putStrLn "    :k type - Infer the type of a type (its kind)"
   putStrLn "    :l path - Load the file"
   putStrLn "    :r      - Reload the currenty loaded file"
   putStrLn "    :q      - Quit"
